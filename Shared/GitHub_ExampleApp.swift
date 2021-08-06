82e89bcae476b290739b1f59763aa1dd184446f7
connorPlank <connoreplank@gmail.com>
2021-08-06 13:51:25 -0500

    Initial Commit

---
+++ Shared/GitHub_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExampleApp.swift
+//  Shared
+//
+//  Created by Connor Plank on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
